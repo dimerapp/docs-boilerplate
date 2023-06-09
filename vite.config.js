7040add3f738d6135edf221eab6eee8c5004e576
Harminder Virk <virk.officials@gmail.com>
2023-06-09 13:08:09 +0530

    refactor: update scripts to download sponsors

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import Adonis from '@adonisjs/vite/plugin'
 export default defineConfig({
   plugins: [Adonis({
     entrypoints: ['./assets/app.js', './assets/app.css'],
-    reload: ['content/**/*', 'templates/**/*.edge'],
-    publicDirectory: 'dist'
+    reload: ['content/**/*', 'templates/**/*.edge']
   })]
 })
